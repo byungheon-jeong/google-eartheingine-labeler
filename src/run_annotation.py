3f7584d425ec9a289e31cddc72de4cd77e0f4d87
byungheon-jeong <45370147+byungheon-jeong@users.noreply.github.com>
2022-02-10 18:51:20 -0800

    Update run_annotation.py
    fix checkpoint system

--- src/run_annotation.py
+++ src/run_annotation.py
@@ -1,17 +1,8 @@
-import os,time, pickle, re
-from tkinter.messagebox import NO
-from unittest.case import TestCase
+import os,time, pickle, re, json
 import rasterio #for reading images
 import napari, unittest
-
-import multiprocessing as mlt
 import numpy as np
-import matplotlib.pyplot as plt 
-import pandas as pd
-
 from matplotlib import path
-from scipy.sparse import data
-from argparse import ArgumentParser
 
 directory = os.path.join(os.getcwd(), "ee_data")
 
@@ -141,13 +132,14 @@ def loadCheckpoint(directory,num_bands):
         directory (str): path of directory
         num_bands (int): number of bands within full TIFF
     """
-    logpath = os.path.join(directory,"checkpoints", "imgAnnotatedData.npy")
+    logpath = os.path.join(directory,"checkpoints", "imgAnnotatedData.json")
+    imageList = set([])
     try:
-        with open(logpath, "rb") as f:
-            imageList = np.load(f)
+        with open(logpath, "r") as f:
+            imageList = json.loads(f.read())
+            imageList = set(imageList)
 
     except (OSError, IOError) as e:
-        imageList = np.array([])
         try :
             os.mkdir(os.path.join(directory,"checkpoints"))
         except:
@@ -173,9 +165,10 @@ def updateLog(imageList,directory):
         imageList (list): contains all images
         directory (str): path to the directory with script and checkpoint files
     """
-    logpath = os.path.join(directory, "imgAnnotatedData.npy")
-    with open(logpath, "wb") as f:
-        np.save(f, imageList)
+    logpath = os.path.join(directory, "imgAnnotatedData.json")
+    imageList = list(imageList)
+    with open(logpath, "w") as f:
+        f.write(json.dumps(imageList))
 
 
 def updateArraysAndSave(data,image_data,labels,image_labels,directory):
@@ -198,7 +191,7 @@ def updateArraysAndSave(data,image_data,labels,image_labels,directory):
         np.save(f, labels)
 
 
-def runTestsAndLog(coordiantes, viewer, annotated_list,image_file,checkpoint_directory):
+def runTests(coordiantes, viewer):
     """
      Waits for user input to cycle through images
 
@@ -211,8 +204,6 @@ def runTestsAndLog(coordiantes, viewer, annotated_list,image_file,checkpoint_dir
     """
     testPixelMask(coordiantes, viewer)
     input("Input ENTER after checking RB pixels")
-    annotated_list = np.append(annotated_list, image_file)
-    updateLog(annotated_list, checkpoint_directory)
 
 
 def testPixelMask(coordiantes,viewer):
@@ -237,7 +228,6 @@ def main():
     full_data_directory = os.path.join(directory, "full_img")
     checkpointdirectory = os.path.join(directory, "checkpoints")
     num_bands = rasterio.open(os.path.join(full_data_directory,list(os.walk(full_data_directory))[0][-1][-1])).read().shape[0]
-    annotated_list, data, labels = loadCheckpoint(directory,num_bands)
 
     # Iterate through the images in directory
     for root, dirs, files in os.walk(image_directory):
@@ -249,7 +239,8 @@ def main():
             # Get the images that have all the bands
             full_image_path = os.path.join(full_data_directory, image_file)
             print(image_path)
-            if os.path.splitext(image_path)[1] == ".tif" and image_file not in annotated_list:
+            # if os.path.splitext(image_path)[1] == ".tif" and image_file not in annotated_list:
+            if image_path not in annotated_list:
                 viewer,img, img_full = runNapari(image_path,full_image_path)
                 flag = True
                 while flag:
@@ -257,7 +248,7 @@ def main():
                     # If the TIFF image is too distorted or otherwise unusable
                     if response == "SKIP":
                         viewer.close()
-                        annotated_list = np.append(annotated_list, image_path)
+                        annotated_list.add(image_path)
                         updateLog(annotated_list, checkpointdirectory)
                         break
                     try:    
@@ -272,8 +263,10 @@ def main():
                     
                     if not flag:
                         try:
-                            runTestsAndLog(coordinates, viewer, annotated_list,image_file,checkpointdirectory)
+                            runTests(coordinates, viewer)
                             updateArraysAndSave(data,image_data,labels,image_labels,checkpointdirectory)
+                            annotated_list.add(image_path)
+                            updateLog(annotated_list, checkpointdirectory)
                             viewer.close()
                         except Exception as e:
                             print(e)
